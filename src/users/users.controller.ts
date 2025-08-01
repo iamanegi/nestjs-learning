104228840545655fd1251005441654b10c76644f
Aman Singh Negi <amannegi229@gmail.com>
2025-08-01 17:04:03 +0530

    add controller and service

---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get('/{:id}')
+  getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    return `Hello User ${id} ${limit} ${page}`;
+  }
+}
