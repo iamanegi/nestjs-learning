d4c36119b87c44f03b52da80feb48725cd8d18c5
Aman Singh Negi <amannegi229@gmail.com>
2025-08-01 17:39:36 +0530

    param dto validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,16 +9,17 @@ import {
   Post,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/{:id}')
   getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    return `Hello User ${id} ${limit} ${page}`;
+    return `Hello User ${JSON.stringify(getUsersParamDto)} ${limit} ${page}`;
   }
 
   @Post()
