2e25ba9bebe1c6d0dd90c29fdbb2e4a073da27c2
Aman Singh Negi <amannegi229@gmail.com>
2025-08-02 21:24:43 +0530

    get method documentation

--- src/users/dtos/get-users-param.dto.ts
+++ src/users/dtos/get-users-param.dto.ts
@@ -1,7 +1,12 @@
 import { IsInt, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class GetUsersParamDto {
+  @ApiPropertyOptional({
+    description: 'The id of the user',
+    example: 1,
+  })
   @IsOptional()
   @IsInt()
   @Type(() => Number)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,12 +13,32 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/{:id}')
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'Users fetched successfully.',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: Number,
+    required: false,
+    description: 'Item count per page',
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'page',
+    type: Number,
+    required: false,
+    description: 'Page number',
+    example: 1,
+  })
   getUsers(
     @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
