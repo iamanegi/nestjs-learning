b7b1150faedfd129fe5bfa9066b920823bd8dc14
Aman Singh Negi <amannegi229@gmail.com>
2025-08-02 21:24:20 +0530

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
       transform: true, // transform the request body to the DTO type
     }),
   );
+  // swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Blogs API')
+    .setDescription('Blogs API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
