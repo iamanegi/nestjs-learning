2b58c93e4b2869647ac3377f01f13bf04697ed7a
Aman Singh Negi <amannegi229@gmail.com>
2025-08-07 23:29:28 +0530

    add create post method documentation

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -15,17 +15,28 @@ import { PostType } from '../enums/post-type.enum';
 import { PostStatus } from '../enums/post-status.enum';
 import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'The title of the post',
+    example: 'My First Post',
+    minLength: 4,
+  })
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ enum: PostType })
   @IsEnum(PostType)
   @IsNotEmpty()
   postType: PostType;
 
+  @ApiProperty({
+    description: 'The slug of the post',
+    example: 'my-first-post',
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
@@ -34,32 +45,66 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty({ enum: PostStatus })
   @IsEnum(PostStatus)
   @IsNotEmpty()
   status: PostStatus;
 
+  @ApiPropertyOptional({
+    description: 'The content of the post',
+    example: 'This is the content of my first post.',
+  })
   @IsOptional()
   @IsString()
   content?: string;
 
+  @ApiPropertyOptional({
+    description: 'The json schema for the post',
+    example: '{"key":"value"}',
+  })
   @IsOptional()
   @IsJSON()
   schema?: string;
 
+  @ApiPropertyOptional({
+    description: 'The featured image url of the post',
+    example: 'https://example.com/image.jpg',
+  })
   @IsOptional()
   @IsUrl()
   featuredImageUrl?: string;
 
+  @ApiPropertyOptional({
+    description: 'The date and time the post will be published',
+    example: '2021-01-01T00:00:00.000Z',
+  })
   @IsOptional()
   @IsISO8601()
   publishOn?: Date;
 
+  @ApiPropertyOptional({
+    description: 'The tags of the post',
+    example: ['tag1', 'tag2'],
+  })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   tags?: string[];
 
+  @ApiPropertyOptional({
+    type: 'array',
+    required: false,
+    items: {
+      type: 'object',
+      properties: {
+        key: { type: 'string', description: 'The key of the meta option' },
+        value: { type: 'any', description: 'The value of the meta option' },
+      },
+    },
+    description: 'The meta options of the post',
+    example: [{ key: 'key1', value: 'value1' }],
+  })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('posts')
 export class PostsController {
@@ -11,6 +12,13 @@ export class PostsController {
     return this.postsService.findAll(userId);
   }
 
+  @ApiOperation({
+    summary: 'Create a new post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The post has been successfully created.',
+  })
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     console.log(createPostDto);
