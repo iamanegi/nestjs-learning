e8d1cd8d004abfe383af87624737187cd3bf20aa
Aman Singh Negi <amannegi229@gmail.com>
2025-08-09 17:25:34 +0530

    add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'aman',
+      password: '',
+      database: 'blog',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
