cb2f041b18d9dbf8b91477a535784829ae2c832b
Aman Singh Negi <amannegi229@gmail.com>
2025-08-10 23:27:22 +0530

    handle get posts method

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -18,22 +18,9 @@ export class PostsService {
     return await this.postsRepository.save(post);
   }
 
-  public findAll(userId: string) {
+  public async findAll(userId: string) {
     const user = this.usersService.findOneById(userId);
-    console.log('findAll', user);
-    return [
-      {
-        id: 1,
-        title: 'Post 1',
-        content: 'Content 1',
-        user: user,
-      },
-      {
-        id: 2,
-        title: 'Post 2',
-        content: 'Content 2',
-        user: user,
-      },
-    ];
+    console.log(user);
+    return await this.postsRepository.find();
   }
 }
