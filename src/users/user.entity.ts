0e913437e92a8064e7e4f8c89ecae02e54b1b992
Aman Singh Negi <amannegi229@gmail.com>
2025-08-10 11:39:20 +0530

    add user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id: number;
+
+  @Column({
+    name: 'first_name',
+    type: 'varchar',
+    length: 20,
+    nullable: false,
+  })
+  firstName: string;
+
+  @Column({
+    name: 'last_name',
+    type: 'varchar',
+    length: 20,
+    nullable: true,
+  })
+  lastName: string;
+
+  @Column({
+    name: 'email',
+    type: 'varchar',
+    length: 90,
+    nullable: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    name: 'password',
+    type: 'varchar',
+    length: 50,
+    nullable: false,
+  })
+  password: string;
+}
