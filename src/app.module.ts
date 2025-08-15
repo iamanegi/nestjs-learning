a65649a7c7a2cfbb90652d2ca7b7e8cbda92219c
Aman Singh Negi <amannegi229@gmail.com>
2025-08-15 23:13:11 +0530

    Add database configuration to app.config.ts and refactor app.module.ts to utilize new configuration structure; update .env.development for additional settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfig } from './config/app.config';
 
 const ENV = process.env.NODE_ENV;
 
@@ -20,19 +21,22 @@ const ENV = process.env.NODE_ENV;
       isGlobal: true, // enable config module for all the modules
       // envFilePath: ['.env.development'],
       envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [AppConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: configService.get<boolean>(
+          'database.autoLoadEntities',
+        ),
+        synchronize: configService.get<boolean>('database.synchronize'),
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
       }),
     }),
     TagsModule,
