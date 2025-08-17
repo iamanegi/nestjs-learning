c1318add3e8ae29f35076078929a8f0a03d0dc76
Aman Singh Negi <amannegi229@gmail.com>
2025-08-17 13:37:36 +0530

    Add implicit conversion option in DTO transformation settings for improved query parameter handling

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
       whitelist: true, // remove any properties that are not in the DTO
       forbidNonWhitelisted: true, // throw an error if a property that is not in the DTO is sent
       transform: true, // transform the request body to the DTO type
+      transformOptions: {
+        enableImplicitConversion: true, // enable implicit conversion of query parameters to the DTO type
+      },
     }),
   );
   // swagger
