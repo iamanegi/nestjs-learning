556bf18ece4d2f61f472c5ad3bb4417af7f37bc0
Aman Singh Negi <amannegi229@gmail.com>
2025-08-24 20:35:48 +0530

    Add global interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,9 +14,10 @@ import databaseConfig from './config/database.config';
 import environmentValidation from './config/environment.validation';
 import jwtConfig from './auth/config/jwt.config';
 import { JwtModule } from '@nestjs/jwt';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AccessTokenGuard } from './auth/guards/access-token/access-token.guard';
 import { AuthGuard } from './auth/guards/auth/auth.guard';
+import { DataResponseInterceptor } from './common/interceptors/data-response.interceptor';
 
 const ENV = process.env.NODE_ENV;
 
@@ -58,6 +59,10 @@ const ENV = process.env.NODE_ENV;
   providers: [
     AppService,
     { provide: APP_GUARD, useClass: AuthGuard },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: DataResponseInterceptor,
+    },
     AccessTokenGuard,
   ],
 })
---
+++ src/common/interceptors/data-response.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DataResponseInterceptor implements NestInterceptor {
+  constructor(private readonly configService: ConfigService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        api_version: this.configService.get('appConfig.apiVersion') as string,
+        data: data as unknown,
+      })),
+    );
+  }
+}
