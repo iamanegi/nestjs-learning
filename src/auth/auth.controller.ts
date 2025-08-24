50048e5acab892f435cbf0fee5a3661e42e70c04
Aman Singh Negi <amannegi229@gmail.com>
2025-08-24 14:23:32 +0530

    Add AuthType enum and Auth decorator for flexible authentication handling. Update sign-in endpoint to utilize new Auth decorator with NONE type. Introduce AUTH_TYPE_KEY constant for metadata management.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dtos/signin.dto';
+import { AuthType } from './enums/auth-type.enum';
+import { Auth } from './decorators/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
+  @Auth(AuthType.NONE)
   @HttpCode(HttpStatus.OK)
   public signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
